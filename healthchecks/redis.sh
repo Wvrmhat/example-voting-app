4d33c70a6b898680af359033846f8ebcf86d85a1
Wvrmhat <86238625+Wvrmhat@users.noreply.github.com>
2025-04-06 21:07:57 +0200

    Updated redis.sh

--- healthchecks/redis.sh
+++ healthchecks/redis.sh
@@ -4,8 +4,13 @@ set -eo pipefail
 
 host="$(hostname -i || echo '127.0.0.1')"
 
-if ping="$(redis-cli -h "$host" ping)" && [ "$ping" = 'PONG' ]; then
-	exit 0
-fi
+for i in {1..5}; do
+	if ping="$(redis-cli -h "$host" ping)" && [ "$ping" = 'PONG' ]; then
+		exit 0
+	fi
+	echo "Attempt  $i/5 failed. Retrying in 2 seconds..."
+	sleep 2
+	
+done
 
 exit 1
